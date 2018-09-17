cabefc494e41bc7f8959cda9169171afc3d3aa93
Trevor Morris <trevor.morris@machinemetrics.com>
2018-09-17 19:27:02 -0400

    rune translation

--- main.go
+++ main.go
@@ -1,13 +1,45 @@
 package main
 
 import (
-	mime
-	os
-	unicode
-	path
-	debug
+  "fmt"
+//  "mime"
 )
 
 func main() {
+//  str := mime.BEncoding.Encode("utf-8", "¡Hola, señor!")
+  str := "01234567890abcdefghijklmnopqrstuvwxyz??\n01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  fmt.Println(str);
+  emo := stringToEmoji(str);
+  fmt.Println(emo);
+}
 
+func stringToEmoji(str string) string {
+  runes := []rune(str)
+  outrunes := make([]rune, len(runes))
+  for i := 0; i < len(runes); i = i+1 {
+    outrunes[i] = translateRune(runes[i])
+  }
+  return string(outrunes)
+}
+
+func translateRune(r rune) rune {
+  var lowerA = '😄'
+  if (r >= 'a' && r <= 'z') {
+    return lowerA + (r - 'a');
+  }
+  var capA = '😤'
+  if (r >= 'A' && r <= 'Z') {
+    return capA + (r - 'A');
+  }
+  var num = '🤐'
+  fmt.Println(int(lowerA))
+  fmt.Println(int(capA))
+  fmt.Println(int(num))
+  if (r >= '0' && r <= '9') {
+    return num + (r - '0');
+  }
+  if (r == '?') {
+    return '💥'
+  }
+  return r
 }
